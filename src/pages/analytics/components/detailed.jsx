060f1336d63a8307037520ad9a8213a0c8ec5c7e
iamcoderdevesh <deveshukalkar007@gmail.com>
2023-10-22 06:37:56 +0530

    Day 16 - Detailed Section - Detailed Section of Analytics Page Designed with demo data. - Added Positive & Negative bar chart

--- src/pages/analytics/components/detailed.jsx
+++ src/pages/analytics/components/detailed.jsx
@@ -1,9 +1,66 @@
 import React from 'react'
+import DataTable from 'components/common/table';
+import { AiFillCaretUp } from "react-icons/ai";
+import ReturnsChart from 'components/charts/ReturnsChart';
 
 const Detailed = () => {
+
+  const data = [
+    {
+      Symbol: "Apple MacBook Pro 17",
+      Status: "WIN",
+    },
+    {
+      Symbol: "Microsoft Surface Pro",
+      Status: "LOSS",
+    },
+    {
+      Symbol: "Magic Mouse 2",
+      Status: "LOSS",
+    },
+  ]
+
+  const columns = [
+    {
+      accessorKey: "Symbol",
+      header: () => <span>Symbol</span>,
+      cell: ({ row }) => <div className="capitalize">{row.getValue("Symbol")}</div>
+    },
+    {
+      accessorKey: "Status",
+      header: () => <span>Status</span>,
+      cell: ({ row }) => <div className="capitalize">{row.getValue("Status")}</div>
+    },
+  ]
+
   return (
-    <div>
-    </div>
+    <>
+      <div className="h-auto my-4 bg-white dark:bg-main-dark rounded-md shadow-sm">
+        <div className="overflow-hidden min-w-max">
+          <div className='headings p-4'>
+            <h3 className="text-base font-semibold dark:text-white">YOUR STATS</h3>
+            <span className="text-sm font-medium dark:text-white">(All Dates)</span>
+          </div>
+          <div className="grid gap-y-6 md:grid-cols-2 p-4 text-sm font-medium gap-x-8">
+            <DataTable data={data} columns={columns} pagination={false} />
+            <DataTable data={data} columns={columns} pagination={false} />
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-col">
+        <h3 className='text-base font-medium dark:text-white mb-2'>Daily Returns</h3>
+        <div className="rounded-md border border-gray-200 dark:border-gray-900 shadow-sm bg-white dark:bg-main-dark">
+          <div className="flex m-4 mt-6">
+            <AiFillCaretUp className="text-green mr-1" />
+            <h5 className="leading-none text-base font-medium text-green pb-2">$2,450 (45.67%)</h5>
+            <p className="text-base font-normal text-gray-500 dark:text-gray-400"></p>
+          </div>
+          <div className="w-full bg-white rounded-lg shadow dark:bg-main-dark p-4 md:p-6 md:pt-0">
+            <ReturnsChart />
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
--- src/pages/trades/TradeLog/index.jsx
+++ src/pages/trades/TradeLog/index.jsx
@@ -4,8 +4,501 @@ import { AiOutlineLineChart } from "react-icons/ai";
 import { LuDollarSign } from "react-icons/lu";
 import { MdOutlineBarChart } from "react-icons/md";
 import { PiChartLineUpBold } from "react-icons/pi";
+import { classNames } from "components/utils";
+
+import { RiArrowUpDownLine } from 'react-icons/ri';
+import { FaArrowUp, FaArrowDown } from 'react-icons/fa6';
+
+import Checkbox from "components/common/checkbox";
+import StatusPill from "components/common/statusPill";
 
 const TadeLogs = () => {
+
+    const data = [
+        {
+            Symbol: "NIFTY",
+            Status: "WIN",
+            "Open Date": "10/18/2023",
+            "Close Date": "10/18/2023",
+            Action: "BUY / LONG",
+            "Net PnL": "+₹500.00",
+            "Net ROI": "+5.00%",
+            "Entry Price": "₹100.00",
+            "Exit Price": "₹110.00",
+            "Stop Loss": "₹90.00",
+            Quantity: "50.00",
+            Setup: "5EMA",
+            "Entry Reason": "Setup Created",
+            "Exit Reason": "Target Hit",
+            Emotions: "Normal",
+            "Additional Info": "",
+        },
+        {
+            Symbol: "NIFTY",
+            Status: "LOSS",
+            "Open Date": "10/16/2023",
+            "Close Date": "10/16/2023",
+            "Net PnL": "-₹500.00",
+            "Net ROI": "-5.00%",
+            Action: "BUY / LONG",
+            "Entry Price": "₹200.00",
+            "Exit Price": "₹190.00",
+            "Stop Loss": "₹190.00",
+            Quantity: "50.00",
+            Setup: "5EMA",
+            "Entry Reason": "Setup Created",
+            "Exit Reason": "Stop Loss Hit",
+            Emotions: "Normal",
+            "Additional Info": "",
+        },
+        {
+            Symbol: "NIFTY",
+            Status: "WIN",
+            "Open Date": "10/18/2023",
+            "Close Date": "10/18/2023",
+            Action: "BUY / LONG",
+            "Net PnL": "+₹500.00",
+            "Net ROI": "+5.00%",
+            "Entry Price": "₹100.00",
+            "Exit Price": "₹110.00",
+            "Stop Loss": "₹90.00",
+            Quantity: "50.00",
+            Setup: "5EMA",
+            "Entry Reason": "Setup Created",
+            "Exit Reason": "Target Hit",
+            Emotions: "Normal",
+            "Additional Info": "",
+        },
+        {
+            Symbol: "NIFTY",
+            Status: "LOSS",
+            "Open Date": "10/16/2023",
+            "Close Date": "10/16/2023",
+            "Net PnL": "-₹500.00",
+            "Net ROI": "-5.00%",
+            Action: "BUY / LONG",
+            "Entry Price": "₹200.00",
+            "Exit Price": "₹190.00",
+            "Stop Loss": "₹190.00",
+            Quantity: "50.00",
+            Setup: "5EMA",
+            "Entry Reason": "Setup Created",
+            "Exit Reason": "Stop Loss Hit",
+            Emotions: "Normal",
+            "Additional Info": "",
+        },
+        {
+            Symbol: "NIFTY",
+            Status: "WIN",
+            "Open Date": "10/18/2023",
+            "Close Date": "10/18/2023",
+            Action: "BUY / LONG",
+            "Net PnL": "+₹500.00",
+            "Net ROI": "+5.00%",
+            "Entry Price": "₹100.00",
+            "Exit Price": "₹110.00",
+            "Stop Loss": "₹90.00",
+            Quantity: "50.00",
+            Setup: "5EMA",
+            "Entry Reason": "Setup Created",
+            "Exit Reason": "Target Hit",
+            Emotions: "Normal",
+            "Additional Info": "",
+        },
+        {
+            Symbol: "NIFTY",
+            Status: "LOSS",
+            "Open Date": "10/16/2023",
+            "Close Date": "10/16/2023",
+            "Net PnL": "-₹500.00",
+            "Net ROI": "-5.00%",
+            Action: "BUY / LONG",
+            "Entry Price": "₹200.00",
+            "Exit Price": "₹190.00",
+            "Stop Loss": "₹190.00",
+            Quantity: "50.00",
+            Setup: "5EMA",
+            "Entry Reason": "Setup Created",
+            "Exit Reason": "Stop Loss Hit",
+            Emotions: "Normal",
+            "Additional Info": "",
+        },
+        {
+            Symbol: "NIFTY",
+            Status: "WIN",
+            "Open Date": "10/18/2023",
+            "Close Date": "10/18/2023",
+            Action: "BUY / LONG",
+            "Net PnL": "+₹500.00",
+            "Net ROI": "+5.00%",
+            "Entry Price": "₹100.00",
+            "Exit Price": "₹110.00",
+            "Stop Loss": "₹90.00",
+            Quantity: "50.00",
+            Setup: "5EMA",
+            "Entry Reason": "Setup Created",
+            "Exit Reason": "Target Hit",
+            Emotions: "Normal",
+            "Additional Info": "",
+        },
+        {
+            Symbol: "NIFTY",
+            Status: "LOSS",
+            "Open Date": "10/16/2023",
+            "Close Date": "10/16/2023",
+            "Net PnL": "-₹500.00",
+            "Net ROI": "-5.00%",
+            Action: "BUY / LONG",
+            "Entry Price": "₹200.00",
+            "Exit Price": "₹190.00",
+            "Stop Loss": "₹190.00",
+            Quantity: "50.00",
+            Setup: "5EMA",
+            "Entry Reason": "Setup Created",
+            "Exit Reason": "Stop Loss Hit",
+            Emotions: "Normal",
+            "Additional Info": "",
+        },
+        {
+            Symbol: "NIFTY",
+            Status: "WIN",
+            "Open Date": "10/18/2023",
+            "Close Date": "10/18/2023",
+            Action: "BUY / LONG",
+            "Net PnL": "+₹500.00",
+            "Net ROI": "+5.00%",
+            "Entry Price": "₹100.00",
+            "Exit Price": "₹110.00",
+            "Stop Loss": "₹90.00",
+            Quantity: "50.00",
+            Setup: "5EMA",
+            "Entry Reason": "Setup Created",
+            "Exit Reason": "Target Hit",
+            Emotions: "Normal",
+            "Additional Info": "",
+        },
+        {
+            Symbol: "NIFTY",
+            Status: "LOSS",
+            "Open Date": "10/16/2023",
+            "Close Date": "10/16/2023",
+            "Net PnL": "-₹500.00",
+            "Net ROI": "-5.00%",
+            Action: "BUY / LONG",
+            "Entry Price": "₹200.00",
+            "Exit Price": "₹190.00",
+            "Stop Loss": "₹190.00",
+            Quantity: "50.00",
+            Setup: "5EMA",
+            "Entry Reason": "Setup Created",
+            "Exit Reason": "Stop Loss Hit",
+            Emotions: "Normal",
+            "Additional Info": "",
+        },
+        {
+            Symbol: "NIFTY",
+            Status: "WIN",
+            "Open Date": "10/18/2023",
+            "Close Date": "10/18/2023",
+            Action: "BUY / LONG",
+            "Net PnL": "+₹500.00",
+            "Net ROI": "+5.00%",
+            "Entry Price": "₹100.00",
+            "Exit Price": "₹110.00",
+            "Stop Loss": "₹90.00",
+            Quantity: "50.00",
+            Setup: "5EMA",
+            "Entry Reason": "Setup Created",
+            "Exit Reason": "Target Hit",
+            Emotions: "Normal",
+            "Additional Info": "",
+        },
+        {
+            Symbol: "NIFTY",
+            Status: "LOSS",
+            "Open Date": "10/16/2023",
+            "Close Date": "10/16/2023",
+            "Net PnL": "-₹500.00",
+            "Net ROI": "-5.00%",
+            Action: "BUY / LONG",
+            "Entry Price": "₹200.00",
+            "Exit Price": "₹190.00",
+            "Stop Loss": "₹190.00",
+            Quantity: "50.00",
+            Setup: "5EMA",
+            "Entry Reason": "Setup Created",
+            "Exit Reason": "Stop Loss Hit",
+            Emotions: "Normal",
+            "Additional Info": "",
+        },
+        {
+            Symbol: "NIFTY",
+            Status: "WIN",
+            "Open Date": "10/18/2023",
+            "Close Date": "10/18/2023",
+            Action: "BUY / LONG",
+            "Net PnL": "+₹500.00",
+            "Net ROI": "+5.00%",
+            "Entry Price": "₹100.00",
+            "Exit Price": "₹110.00",
+            "Stop Loss": "₹90.00",
+            Quantity: "50.00",
+            Setup: "5EMA",
+            "Entry Reason": "Setup Created",
+            "Exit Reason": "Target Hit",
+            Emotions: "Normal",
+            "Additional Info": "",
+        },
+        {
+            Symbol: "NIFTY",
+            Status: "LOSS",
+            "Open Date": "10/16/2023",
+            "Close Date": "10/16/2023",
+            "Net PnL": "-₹500.00",
+            "Net ROI": "-5.00%",
+            Action: "BUY / LONG",
+            "Entry Price": "₹200.00",
+            "Exit Price": "₹190.00",
+            "Stop Loss": "₹190.00",
+            Quantity: "50.00",
+            Setup: "5EMA",
+            "Entry Reason": "Setup Created",
+            "Exit Reason": "Stop Loss Hit",
+            Emotions: "Normal",
+            "Additional Info": "",
+        },
+        {
+            Symbol: "NIFTY",
+            Status: "WIN",
+            "Open Date": "10/18/2023",
+            "Close Date": "10/18/2023",
+            Action: "BUY / LONG",
+            "Net PnL": "+₹500.00",
+            "Net ROI": "+5.00%",
+            "Entry Price": "₹100.00",
+            "Exit Price": "₹110.00",
+            "Stop Loss": "₹90.00",
+            Quantity: "50.00",
+            Setup: "5EMA",
+            "Entry Reason": "Setup Created",
+            "Exit Reason": "Target Hit",
+            Emotions: "Normal",
+            "Additional Info": "",
+        },
+        {
+            Symbol: "NIFTY",
+            Status: "LOSS",
+            "Open Date": "10/16/2023",
+            "Close Date": "10/16/2023",
+            "Net PnL": "-₹500.00",
+            "Net ROI": "-5.00%",
+            Action: "BUY / LONG",
+            "Entry Price": "₹200.00",
+            "Exit Price": "₹190.00",
+            "Stop Loss": "₹190.00",
+            Quantity: "50.00",
+            Setup: "5EMA",
+            "Entry Reason": "Setup Created",
+            "Exit Reason": "Stop Loss Hit",
+            Emotions: "Normal",
+            "Additional Info": "",
+        },
+        {
+            Symbol: "NIFTY",
+            Status: "WIN",
+            "Open Date": "10/18/2023",
+            "Close Date": "10/18/2023",
+            Action: "BUY / LONG",
+            "Net PnL": "+₹500.00",
+            "Net ROI": "+5.00%",
+            "Entry Price": "₹100.00",
+            "Exit Price": "₹110.00",
+            "Stop Loss": "₹90.00",
+            Quantity: "50.00",
+            Setup: "5EMA",
+            "Entry Reason": "Setup Created",
+            "Exit Reason": "Target Hit",
+            Emotions: "Normal",
+            "Additional Info": "",
+        },
+        {
+            Symbol: "NIFTY",
+            Status: "LOSS",
+            "Open Date": "10/16/2023",
+            "Close Date": "10/16/2023",
+            "Net PnL": "-₹500.00",
+            "Net ROI": "-5.00%",
+            Action: "BUY / LONG",
+            "Entry Price": "₹200.00",
+            "Exit Price": "₹190.00",
+            "Stop Loss": "₹190.00",
+            Quantity: "50.00",
+            Setup: "5EMA",
+            "Entry Reason": "Setup Created",
+            "Exit Reason": "Stop Loss Hit",
+            Emotions: "Normal",
+            "Additional Info": "",
+        },
+        {
+            Symbol: "NIFTY",
+            Status: "WIN",
+            "Open Date": "10/18/2023",
+            "Close Date": "10/18/2023",
+            Action: "BUY / LONG",
+            "Net PnL": "+₹500.00",
+            "Net ROI": "+5.00%",
+            "Entry Price": "₹100.00",
+            "Exit Price": "₹110.00",
+            "Stop Loss": "₹90.00",
+            Quantity: "50.00",
+            Setup: "5EMA",
+            "Entry Reason": "Setup Created",
+            "Exit Reason": "Target Hit",
+            Emotions: "Normal",
+            "Additional Info": "",
+        },
+        {
+            Symbol: "NIFTY",
+            Status: "LOSS",
+            "Open Date": "10/16/2023",
+            "Close Date": "10/16/2023",
+            "Net PnL": "-₹500.00",
+            "Net ROI": "-5.00%",
+            Action: "BUY / LONG",
+            "Entry Price": "₹200.00",
+            "Exit Price": "₹190.00",
+            "Stop Loss": "₹190.00",
+            Quantity: "50.00",
+            Setup: "5EMA",
+            "Entry Reason": "Setup Created",
+            "Exit Reason": "Stop Loss Hit",
+            Emotions: "Normal",
+            "Additional Info": "",
+        },
+    ]
+
+    const columns = [
+        {
+            id: "select",
+            header: ({ table }) => (
+                <Checkbox
+                    checked={table.getIsAllPageRowsSelected()}
+                    onChange={table.getToggleAllRowsSelectedHandler()}
+                    aria-label="Select all"
+                />
+            ),
+            cell: ({ row }) => (
+                <Checkbox
+                    checked={row.getIsSelected()}
+                    onChange={row.getToggleSelectedHandler()}
+                    aria-label="Select row"
+                />
+            ),
+            enableSorting: false,
+            enableHiding: false
+        },
+        {
+            accessorKey: "Symbol",
+            header: () => <span>Symbol</span>,
+            cell: ({ row }) => <div className="capitalize">{row.getValue("Symbol")}</div>
+        },
+        {
+            accessorKey: "Status",
+            header: ({ column }) => {
+                return (
+                    <button
+                        className="flex items-center justify-center"
+                        variant="ghost"
+                        onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+                    >
+                        Status
+                        {column.getIsSorted() === "desc" ? (
+                            <FaArrowDown className="ml-2 w-4 h-4" />
+                        ) : column.getIsSorted() === "asc" ? (
+                            <FaArrowUp className="ml-2 w-4 h-4" />
+                        ) : (
+                            <RiArrowUpDownLine className="ml-2 h-4 w-4" />
+                        )}
+                    </button>
+                )
+            },
+            cell: ({ row }) => (
+                <div className="capitalize">
+                    <StatusPill value={row.getValue("Status")} />
+                </div>
+            )
+        },
+        {
+            accessorKey: "Open Date",
+            header: () => <div className="capitalize">Open Date</div>,
+            cell: ({ row }) => <div className="capitalize">{row.getValue("Open Date")}</div>
+        },
+        {
+            accessorKey: "Close Date",
+            header: () => <div className="capitalize">Close Date</div>,
+            cell: ({ row }) => <div className="capitalize">{row.getValue("Close Date")}</div>
+        },
+        {
+            accessorKey: "Action",
+            header: () => <div className="capitalize">Action</div>,
+            cell: ({ row }) => <div className="capitalize">{row.getValue("Action")}</div>
+        },
+        {
+            accessorKey: "Net PnL",
+            header: () => <div className="capitalize">Net PnL</div>,
+            cell: ({ row }) => <div className={classNames("capitalize font-medium", row.getValue("Net PnL").startsWith("-") ? "text-red" : "text-green")}>{row.getValue("Net PnL")}</div>
+        },
+        {
+            accessorKey: "Net ROI",
+            header: () => <div className="capitalize">Net ROI</div>,
+            cell: ({ row }) => <div className={classNames("capitalize font-medium", row.getValue("Net ROI").startsWith("-") ? "text-red" : "text-green")}>{row.getValue("Net ROI")}</div>
+        },
+        {
+            accessorKey: "Entry Price",
+            header: () => <div className="capitalize">Entry Price</div>,
+            cell: ({ row }) => <div className="capitalize">{row.getValue("Entry Price")}</div>
+        },
+        {
+            accessorKey: "Exit Price",
+            header: () => <div className="capitalize">Exit Price</div>,
+            cell: ({ row }) => <div className="capitalize">{row.getValue("Exit Price")}</div>
+        },
+        {
+            accessorKey: "Stop Loss",
+            header: () => <div className="capitalize">Stop Loss</div>,
+            cell: ({ row }) => <div className="capitalize">{row.getValue("Stop Loss")}</div>
+        },
+        {
+            accessorKey: "Quantity",
+            header: () => <div className="capitalize">Quantity</div>,
+            cell: ({ row }) => <div className="capitalize font-medium">{row.getValue("Quantity")}</div>
+        },
+        {
+            accessorKey: "Setup",
+            header: () => <div className="capitalize">Setup</div>,
+            cell: ({ row }) => <div className="capitalize">{row.getValue("Setup")}</div>
+        },
+        {
+            accessorKey: "Entry Reason",
+            header: () => <div className="capitalize">Entry Reason</div>,
+            cell: ({ row }) => <div className="capitalize">{row.getValue("Entry Reason")}</div>
+        },
+        {
+            accessorKey: "Exit Reason",
+            header: () => <div className="capitalize">Exit Reason</div>,
+            cell: ({ row }) => <div className="capitalize">{row.getValue("Exit Reason")}</div>
+        },
+        {
+            accessorKey: "Emotions",
+            header: () => <div className="capitalize">Emotions</div>,
+            cell: ({ row }) => <div className="capitalize">{row.getValue("Emotions")}</div>
+        },
+        {
+            accessorKey: "Additional Info",
+            header: () => <div className="capitalize">Additional Info</div>,
+            cell: ({ row }) => <div className="capitalize">{row.getValue("Additional Info")}</div>
+        },
+    ]
 
     return (
         <section className='h-full'>
@@ -65,7 +558,9 @@ const TadeLogs = () => {
                 </div>
             </div>
             <div className="bg-white dark:bg-main-dark h-full p-2 mt-2">
-                <DataTable />
+                <div className="p-2 mx-auto dark:text-white dark:fill-gray-400">
+                    <DataTable data={data} columns={columns} pagination={true} />
+                </div>
             </div>
         </section>
     )
